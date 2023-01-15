b9494673a503694be2c590ea14e33671bb1cda1b
Alex100688 <alessiozingaro2@gmail.com>
2023-01-15 18:32:18 +0100

    crea l 'header e aggiungi il file comics a config

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="d-flex justify-content-between align-items-center">
+        <img class="my-3" src="{{ Vite::asset('resources/img/dc-logo.png') }}"
+            alt="{{ Vite::asset('resources/img/dc-logo.png') }}">
+        <a class="link-header" href="">CHARACTERS</a>
+        <a class="link-header" href="">COMICS</a>
+        <a class="link-header" href="">MOVIES</a>
+        <a class="link-header" href="">TV</a>
+        <a class="link-header" href="">GAMES</a>
+        <a class="link-header" href="">COLLECTIBLES</a>
+        <a class="link-header" href="">VIDEOS</a>
+        <a class="link-header" href="">FANS</a>
+        <a class="link-header" href="">NEWS</a>
+        <a class="link-header" href="">SHOP</a>
+    </div>
+</div>
+<div class="container-sm">
+    <img class="img-fluid" src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="">
+</div>
